23d6db8c55459993cf51fa641939c1b5aa4e4c64
Maxwell Odri <maxwellodri@gmail.com>
2020-05-27 21:18:17 +1000

    thinkpad

--- dwm/dstatus.sh
+++ dwm/dstatus.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 while true; do
-    xsetroot -name "$(date +%a-%d-%b-%R) BAT: $(acpi | grep "Battery 0" | awk '{print $4}' | sed s/,//)"
+    if [ "$(echo "$(date +%s)%5" | bc)" -eq "0" ]; then
+        battery="BAT: $(acpi | grep 'harg' | tr ' ' '\n' | awk '/%/ {print}' | sed s/,//)"
+        network="NET: $(nmcli device | awk '!/disconnected/&&/connected/ {print $4,"[",$2,"]"}') $(nmcli device wifi | awk '/\*/ {print $9}')"
+
+    fi
+    case $dotfiles_tag in 
+        thinkpad)
+                    OPT="$battery, $network"
+                    ;;
+        *)  
+                    OPT="NO TAG"
+                    ;;
+    esac
+    xsetroot -name "$(date +%a-%d-%b-%R) $OPT"
 	sleep 1
 done
 
