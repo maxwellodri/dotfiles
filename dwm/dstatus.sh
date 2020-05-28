a942d11510b134dad6b33542575e98025d6d3988
Maxwell Odri <maxwellodri@gmail.com>
2020-05-28 13:19:16 +1000

    pc

--- dwm/dstatus.sh
+++ dwm/dstatus.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-    xsetroot -name "$(date +%a-%d-%b-%R) BAT: $(acpi | grep "Battery 0" | awk '{print $4}' | sed s/,//)"
+    xsetroot -name "$(date +%a-%d-%b-%R)" # BAT: $(acpi | grep "Battery 0" | awk '{print $4}' | sed s/,//)"
 	sleep 1
 done
 
