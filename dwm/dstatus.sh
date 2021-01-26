ad9fb53fd11ed08de59fa54ebb2e3abcdd38ac45
Maxwell Odri <maxwellodri@gmail.com>
2021-01-26 17:01:32 +1000

    fixed dwm statusbar

--- dwm/dstatus.sh
+++ dwm/dstatus.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-poll_network() {
-    command -v nmcli &>/dev/null && network="🌐: $(nmcli device | awk '!/disconnected/&&/connected/ {print $4}') $(nmcli device wifi | awk '/\*/ {print $9}')" && [ $network = "🌐:" ] && network="🌐: NONE"
-    echo "$network"
-}
-poll_battery() { 
-    battery="🔋: $(acpi | grep 'harg' | tr ' ' '\n' | awk '/%/ {print}' | sed s/,//)"
-    echo "$battery"
-}
+#poll_network() {
+#    command -v nmcli &>/dev/null && network="🌐: $(nmcli device | awk '!/disconnected/&&/connected/ {print $4}') $(nmcli device wifi | awk '/\*/ {print $9}')" && [ $network = "🌐:" ] && network="🌐: NONE"
+#    echo "$network"
+#}
+#poll_battery() { 
+#    battery="🔋: $(acpi | grep 'harg' | tr ' ' '\n' | awk '/%/ {print}' | sed s/,//)"
+#    echo "$battery"
+#}
+#
+#poll_packages() {
+#    command -v yay &>/dev/null && package="📦: $(pacman -Qu | wc -l)/$(yay -Qmu | wc -l)" #every hour on the hour poll for number of official and aur packages via yay
+#    echo "$package"
+#}
 
-poll_packages() {
-    command -v yay &>/dev/null && package="📦: $(pacman -Qu | wc -l)/$(yay -Qmu | wc -l)" #every hour on the hour poll for number of official and aur packages via yay
-    echo "$package"
-}
-
-battery="$(poll_battery)"
-network="$(poll_network)"
-packages="$(poll_packages)"
+#battery="$(poll_battery)"
+#network="$(poll_network)"
+#packages="$(poll_packages)"
 
 while true; do
-    if [ "$(echo "$(date +%s)%5" | bc)" -eq "0" ]; then
-        battery="$(poll_battery)"
-        network="$(poll_network)"
+    if [ "$(echo "$(date +%s)%5" | bc)" -eq "0" ]; then #every five seconds poll these:
+#        battery="$(poll_battery)"
+#        network="$(poll_network)"
+        continue 
     fi
 
     [ $(date +%M) -eq "0" ] && packages="$(poll_packages)"
     case $dotfiles_tag in 
         thinkpad)
-                    OPT="$battery, $network"
+                    OPT=" $battery, $network"
                     ;;
         pc) 
-                    OPT="$packages, $network"
+                    OPT="" #" $packages, $network"
                     ;;
         *)  
                     OPT="NO TAG"
                     ;;
     esac
-    xsetroot -name "🕒 $(date +%a-%d-%b-%R) $OPT"
+    xsetroot -name "🕒 $(date +%a-%d-%b-%R)$OPT"
 	sleep 1
 done
