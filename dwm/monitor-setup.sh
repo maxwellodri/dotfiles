b263476cd3cd1d9abc530b680446c9dc3768ebe9
Maxwell Odri <maxwellodri@gmail.com>
2021-01-28 18:17:15 +1000

    variable monitor resolutions

--- dwm/monitor-setup.sh
+++ dwm/monitor-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 case $dotfiles_tag in 
     pc) 
-            xrandr --output DisplayPort-2 --primary --mode 2560x1440 --rate 144.00 --output HDMI-A-0 --mode 1920x1080 --rate 60.00 --right-of DisplayPort-2
+        #if [ ! -z $(glxinfo -B | grep NVIDIA) ] then;
+            xrandr --output DP-2 --primary --mode 2560x1440 --rate 144.00 --output HDMI-0 --mode 1920x1080 --rate 60.00 --right-of DP-2 #amd
+            #xrandr --output DisplayPort-2 --primary --mode 2560x1440 --rate 144.00 --output HDMI-A-0 --mode 1920x1080 --rate 60.00 --right-of DisplayPort-2 #amd
             ;;
         *)  
             ;;
