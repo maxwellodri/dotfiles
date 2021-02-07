60c89bd1697e6a9212a8dda4559961352e0f45bd
Maxwell Odri <maxwellodri@gmail.com>
2021-02-07 16:10:01 +1000

    fixed statusbar on thinkpad

--- dwm/dstatus.sh
+++ dwm/dstatus.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-#poll_network() {
-#    command -v nmcli &>/dev/null && network="🌐: $(nmcli device | awk '!/disconnected/&&/connected/ {print $4}') $(nmcli device wifi | awk '/\*/ {print $9}')" && [ $network = "🌐:" ] && network="🌐: NONE"
-#    echo "$network"
-#}
-#poll_battery() { 
-#    battery="🔋: $(acpi | grep 'harg' | tr ' ' '\n' | awk '/%/ {print}' | sed s/,//)"
-#    echo "$battery"
-#}
+poll_network() {
+    command -v nmcli &>/dev/null && network="🌐: $(nmcli device | awk '!/disconnected/&&/connected/ {print $4}') $(nmcli device wifi | awk '/\*/ {print $9}')" && [ $network = "🌐:" ] && network="🌐: NONE"
+    echo "$network"
+}
+poll_battery() { 
+    battery="🔋: $(acpi | awk '{print $4}' | head -n1 | sed s/,//)"
+    echo "$battery"
+}
 #
 #poll_packages() {
 #    command -v yay &>/dev/null && package="📦: $(pacman -Qu | wc -l)/$(yay -Qmu | wc -l)" #every hour on the hour poll for number of official and aur packages via yay
 #    echo "$package"
 #}
 
-#battery="$(poll_battery)"
-#network="$(poll_network)"
+battery="$(poll_battery)"
+network="$(poll_network)"
 #packages="$(poll_packages)"
 
 while true; do
     if [ "$(echo "$(date +%s)%5" | bc)" -eq "0" ]; then #every five seconds poll these:
-#        battery="$(poll_battery)"
-#        network="$(poll_network)"
+        battery="$(poll_battery)"
+        network="$(poll_network)"
         continue 
     fi
 
