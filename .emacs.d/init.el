21c82298b130bd80cd5c2609e1269d2449eb5d84
Maxwell Odri <maxwellodri@gmail.com>
2021-10-06 17:26:09 +1000

    fixed emacs taking up all my space

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,9 @@
 (setq user-emacs-directory "/home/maxwell/.emacs.d/") 
+(setenv "HOME" "/home/maxwell/")
 (require 'package)
 (package-initialize)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
 
 (defconst ha/emacs-directory (concat (getenv "HOME") "/.emacs.d/"))
 (defun ha/emacs-subdirectory (d) (expand-file-name d ha/emacs-directory))
@@ -33,3 +34,56 @@
 
 ;; the rest of the configuration can be found in:
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(server-start)
+
+;;Theming
+(load-theme 'misterioso t)
+;; Remove initial buffer, set index file
+(setq inhibit-startup-message t)
+(setq initial-buffer-choice (concat user-emacs-directory "index.org"))
+;; Hide Scroll bar,menu bar, tool bar
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+
+;; Line numbering
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type t)
+
+;; Display battery for when in full screen mode
+;; (display-battery-mode t)
+
+;; Keybindings
+;; (global-set-key (kbd "<f5>") 'revert-buffer)
+;; (global-set-key (kbd "<f3>") 'org-export-dispatch)
+;; (global-set-key (kbd "<f6>") 'eshell) 
+;; (global-set-key (kbd "<f7>") 'ranger) 
+;; (global-set-key (kbd "<f8>") 'magit) 
+
+
+(setq org-src-fontify-natively t
+    org-src-tab-acts-natively t
+    org-confirm-babel-evaluate nil
+    org-edit-src-content-indentation 0)
+
+
+
+;;Evil Config
+
+;;(use-package evil-search-highlight-persist)
+
+(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-cursor '("green" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("red" bar))
+(setq evil-replace-state-cursor '("red" bar))
+(setq evil-operator-state-cursor '("red" hollow))
+
+;;(require 'evil-search-highlight-persist)
+;;(global-evil-search-highlight-persist t)
+
+
