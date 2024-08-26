2c2e4ca462cf1e62d1450d636d7be69889e9a42f
Maxwell Odri <maxwellodri@gmail.com>
2024-08-26 23:41:32 +1000

    add support for dotfile_tag specific packages

--- helper_scripts/arch_package_install.sh
+++ helper_scripts/arch_package_install.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-#to install: cat $FILE | grep -v # | xargs sudo pacman -S --noconfirm -needed
-grep -o '^[^#]*' archlinux_x86_64_packages | xargs sudo pacman -S --noconfirm --needed
+
+# Initialize an empty variable to store the packages
+packages=""
+
+# Check if the first argument is provided and the file exists
+if [ -n "$1" ] && [ -f "archlinux_x86_64_packages.$1" ]; then
+    packages="$(grep -o '^[^#]*' "archlinux_x86_64_packages.$1") "
+fi
+
+# Add the packages from the main file
+packages="$packages$(grep -o '^[^#]*' archlinux_x86_64_packages)"
+
+# Install all the combined packages
+echo "$packages" | xargs sudo pacman -S --noconfirm --needed
--- installer_main.sh
+++ installer_main.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $(realpath "$0")) #cd to dotfiles dir
 #echo $(realpath .)
-sh helper_scripts/arch_package_install.sh
+sh helper_scripts/arch_package_install.sh $1
 sh helper_scripts/makesymlinks.sh "$1"
 sh helper_scripts/custom_bin_scripts.sh
 sh helper_scripts/firefox_user_prefs.sh
