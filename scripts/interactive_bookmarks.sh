f13e21e83b760b147d64ea8980f20fbf6af2078c
Maxwell Odri <maxwellodri@gmail.com>
2024-09-21 02:44:15 +1000

    update dmenu scripts

--- scripts/interactive_bookmarks.sh
+++ scripts/interactive_bookmarks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 bookarmarks="$(dirname "$0")/web_bookmarks.json"
-cat "$bookarmarks" | jq | grep -v { | grep -v '}' | sed 's/\"//g; s/,$//g' | dmenu -c -l 30 | awk '{print $2}' | xclip -selection clipboard
+cat "$bookarmarks" | jq | grep -v { | grep -v '}' | sed 's/\"//g; s/,$//g' | dmenu -c -l 30 --class "dmenu_bookmarks" | awk '{print $2}' | xclip -selection clipboard
  
