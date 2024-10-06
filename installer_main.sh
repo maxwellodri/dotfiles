0e2fc96b0a25b4c9b0da921d29b0df9bf348d019
Maxwell Odri <maxwellodri@gmail.com>
2024-10-06 11:46:46 +1000

    remove old firefox script

--- installer_main.sh
+++ installer_main.sh
@@ -4,7 +4,7 @@ cd $(dirname $(realpath "$0")) #cd to dotfiles dir
 sh helper_scripts/arch_package_install.sh $1
 sh helper_scripts/makesymlinks.sh "$1"
 sh helper_scripts/custom_bin_scripts.sh
-sh helper_scripts/firefox_user_prefs.sh
+sh helper_scripts/firefox_user.sh
 bash helper_scripts/download_suckless.sh #Should always be last to provide ssh copying msg, e.g. see script
 echo "Now run script 'run_root.sh' as root"
 
