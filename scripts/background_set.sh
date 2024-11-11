a0f433d1a6a73d841fc7c5725cf3d52c3575a944
Maxwell Odri <maxwellodri@gmail.com>
2024-11-11 20:32:17 +1000

    update backgroundsetter

--- scripts/background_set.sh
+++ scripts/background_set.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ -f "$bin/monitor.sh" ] && $bin/monitor.sh
-#feh --no-fehbg --bg-fill "$dotfiles/images/mountains.jpg" "$dotfiles/images/lofi.jpg"
 feh --no-fehbg --bg-fill "$dotfiles/images/mountains.jpg" "$dotfiles/images/valley.png"
+feh --no-fehbg --bg-fill "$dotfiles/images/mountains.jpg" "$dotfiles/images/lofi.jpg"
