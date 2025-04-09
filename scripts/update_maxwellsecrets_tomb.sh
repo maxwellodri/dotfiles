6311fa9ae5f2524df325a21212d71d3185b91760
Maxwell Odri <maxwellodri@gmail.com>
2025-04-09 12:13:44 +1000

    fix update_*tomb.sh - syntax bug

--- scripts/update_maxwellsecrets_tomb.sh
+++ scripts/update_maxwellsecrets_tomb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 
 cd ~/Documents/backup/
 
@@ -34,44 +34,45 @@ update() {
                 echo "Installed kernel:  $installed_kernel"
                 echo "Please reboot your system to use the new kernel."
             fi
-
-            exit 1
         fi
 
-        echo "Tomb is mounted at: $mount_point"
+        exit 1
+    fi
 
-        if [ -f "$mount_point/update.sh" ]; then
-            bash "$mount_point/update.sh"
-        else
-            echo "Script update.sh not found in the tomb."
-        fi
-        tomb close maxwellsecrets
-        echo "Tomb has been updated"
+    echo "Tomb is mounted at: $mount_point"
+
+    if [ -f "$mount_point/update.sh" ]; then
+        bash "$mount_point/update.sh"
+    else
+        echo "Script update.sh not found in the tomb."
+    fi
+    tomb close maxwellsecrets
+    echo "Tomb has been updated"
 }
 
 
-    backup() {
-        local paths=("$@")  # Accepts a list of paths as arguments
-        echo "Creating an archive of tomb and key..."
-        TAR_FILE="maxwellsecrets.tomb.tar.gz"
-        tar -czf "$TAR_FILE" maxwellsecrets.tomb maxwellsecrets.tomb.key
+backup() {
+    local paths=("$@")  # Accepts a list of paths as arguments
+    echo "Creating an archive of tomb and key..."
+    TAR_FILE="maxwellsecrets.tomb.tar.gz"
+    tar -czf "$TAR_FILE" maxwellsecrets.tomb maxwellsecrets.tomb.key
 
-        if [ $? -ne 0 ]; then
-            echo "Error: Failed to create the tar archive."
-            exit 1
-        fi
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create the tar archive."
+        exit 1
+    fi
 
-        for path in "${paths[@]}"; do
-            if [[ "$path" == "dropbox" ]]; then
-                echo "Backing up tar archive to Dropbox..."
-                dbxcli put "$TAR_FILE"
-                if [ $? -eq 0 ]; then
-                    echo "Successfully backed up tar archive to Dropbox."
-                else
-                    echo "Error backing up tar archive to Dropbox."
-                fi
+    for path in "${paths[@]}"; do
+        if [[ "$path" == "dropbox" ]]; then
+            echo "Backing up tar archive to Dropbox..."
+            dbxcli put "$TAR_FILE"
+            if [ $? -eq 0 ]; then
+                echo "Successfully backed up tar archive to Dropbox."
             else
-                echo "Backing up tar archive to $path..."
+                echo "Error backing up tar archive to Dropbox."
+            fi
+        else
+            echo "Backing up tar archive to $path..."
 
             # Check if the path is local or remote
             if [[ -e "$path" ]]; then
@@ -89,76 +90,76 @@ update() {
                 else
                     echo "Error backing up tar archive to $path."
                 fi
-                fi
             fi
-        done
+        fi
+    done
 
-        rm "$TAR_FILE"
-        echo "Backup complete, and local archive removed."
-    }
+    rm "$TAR_FILE"
+    echo "Backup complete, and local archive removed."
+}
 
 
-    if [[ $# -eq 0 ]]; then
-        echo "Error: At least one flag (--update or --backup) is required."
-        exit 1
-    fi
+if [[ $# -eq 0 ]]; then
+    echo "Error: At least one flag (--update or --backup) is required."
+    exit 1
+fi
 
-    update_flag=false
-    backup_flag=false
-    dropbox_flag=true
-    device=""
-    paths=()
+update_flag=false
+backup_flag=false
+dropbox_flag=true
+device=""
+paths=()
 
-    while [[ $# -gt 0 ]]; do
-        case "$1" in
-            --update)
-                update_flag=true
-                shift
-                ;;
-            --backup)
-                backup_flag=true
-                paths+=("maxwell@donnie:~/backups/")  # Path to VPS
-                shift
-                ;;
-            --dev | --path)
-                if [ -n "$2" ] && [[ "$2" != --* ]]; then
-                    device="$2"
-                    # Translate device to mount point
-                    mountpoint=$(findmnt -n -o TARGET "$device")
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --update)
+            update_flag=true
+            shift
+            ;;
+        --backup)
+            backup_flag=true
+            paths+=("maxwell@donnie:~/backups/")  # Path to VPS
+            shift
+            ;;
+        --dev | --path)
+            if [ -n "$2" ] && [[ "$2" != --* ]]; then
+                device="$2"
+                # Translate device to mount point
+                mountpoint=$(findmnt -n -o TARGET "$device")
 
-                    if [ -n "$mountpoint" ]; then
-                        paths+=("$mountpoint")
-                    elif [[ -e "$device" ]]; then
-                        paths+=("$device")
-                    else
-                        echo "Error: Device $device is not mounted."
-                        exit 1
-                    fi
-
-                    shift 2
+                if [ -n "$mountpoint" ]; then
+                    paths+=("$mountpoint")
+                elif [[ -e "$device" ]]; then
+                    paths+=("$device")
                 else
-                    echo "Error: "$1" flag requires a path/device argument."
+                    echo "Error: Device $device is not mounted."
                     exit 1
                 fi
-                ;;
-            --nodropbox)
-                dropbox_flag=false
-                shift
-                ;;
-            *)
-                echo "Unknown option: $1"
+
+                shift 2
+            else
+                echo "Error: "$1" flag requires a path/device argument."
                 exit 1
-                ;;
-        esac
-    done
-    if [ "$dropbox_flag" = true ]; then
-        paths+=("dropbox")
-    fi
+            fi
+            ;;
+        --nodropbox)
+            dropbox_flag=false
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+if [ "$dropbox_flag" = true ]; then
+    paths+=("dropbox")
+fi
 
-    if [ "$update_flag" = true ]; then
-        update
-    fi
+if [ "$update_flag" = true ]; then
+    update
+fi
 
-    if [ "$backup_flag" = true ]; then
-        backup "${paths[@]}"  # Pass the list of paths to backup()
-    fi
+if [ "$backup_flag" = true ]; then
+    backup "${paths[@]}"  # Pass the list of paths to backup()
+fi
