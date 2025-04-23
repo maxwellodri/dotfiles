43e27477c2b48caabf7c69df17fbf5116f5e40fc
Maxwell Odri <maxwellodri@gmail.com>
2025-04-23 22:10:10 +1000

    add autoforward toggle lua script for mpv

---
+++ .config/mpv/scripts/toggle_autoforward.lua
@@ -0,0 +1,27 @@
+local autoplay_enabled = true
+
+-- Function to toggle autoplay state
+function toggle_autoplay()
+    autoplay_enabled = not autoplay_enabled
+    
+    if autoplay_enabled then
+        mp.set_property("keepopen", "no")
+        mp.set_property("image-display-duration", "5")
+        mp.osd_message("Autoplay: Enabled")
+        mp.msg.info("Autoplay enabled")
+    else
+        mp.set_property("keepopen", "yes")
+        mp.set_property("image-display-duration", "inf")
+        mp.osd_message("Autoplay: Disabled")
+        mp.msg.info("Autoplay disabled")
+    end
+end
+
+-- Register key binding (Ctrl+a)
+mp.add_key_binding("Ctrl+a", "toggle_autoplay", toggle_autoplay)
+
+-- Print initial state when mpv starts
+mp.register_event("file-loaded", function()
+    local message = autoplay_enabled and "Autoplay: Enabled" or "Autoplay: Disabled"
+    mp.osd_message(message)
+end)
