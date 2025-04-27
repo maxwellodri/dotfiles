2abc23706b684ceca1a994221c340b1f099cfa3c
Maxwell Odri <maxwellodri@gmail.com>
2025-04-27 12:14:16 +1000

    update toggle_autoforward mpv script (loop if !autoforward)

--- .config/mpv/scripts/toggle_autoforward.lua
+++ .config/mpv/scripts/toggle_autoforward.lua
@@ -1,21 +1,25 @@
-local autoplay_enabled = true
+local autoplay_and_autoforward = true
 
--- Function to toggle autoplay state
-function toggle_autoplay()
-    autoplay_enabled = not autoplay_enabled
+-- Function to toggle autoplay/autoforward state
+function toggle_autoplay_autoforward()
+    autoplay_and_autoforward = not autoplay_and_autoforward
     
-    if autoplay_enabled then
+    if autoplay_and_autoforward then
+        -- Enable autoplay and disable looping
+        mp.set_property("loop-file", "no")
         mp.set_property("keepopen", "no")
         mp.set_property("image-display-duration", "5")
-        mp.osd_message("Autoplay: Enabled")
-        mp.msg.info("Autoplay enabled")
+        mp.osd_message("Autoplay & Autoforward: Enabled")
+        mp.msg.info("Autoplay and autoforward enabled")
     else
+        -- Disable autoplay, enable looping
+        mp.set_property("loop-file", "inf")
         mp.set_property("keepopen", "yes")
         mp.set_property("image-display-duration", "inf")
-        mp.osd_message("Autoplay: Disabled")
-        mp.msg.info("Autoplay disabled")
+        mp.osd_message("Autoplay & Autoforward: Disabled (Looping)")
+        mp.msg.info("Autoplay and autoforward disabled, looping enabled")
     end
 end
 
 -- Register key binding (Ctrl+a)
-mp.add_key_binding("Ctrl+a", "toggle_autoplay", toggle_autoplay)
+mp.add_key_binding("Ctrl+a", "toggle_autoplay_autoforward", toggle_autoplay_autoforward)
