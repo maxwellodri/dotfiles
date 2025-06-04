3e63b9a870d7e16ee496a3137101fea6f24b7321
Maxwell Odri <maxwellodri@gmail.com>
2025-06-04 19:47:37 +1000

    update mpv lua scripts

--- .config/mpv/scripts/toggle_autoforward.lua
+++ .config/mpv/scripts/toggle_autoforward.lua
@@ -8,7 +8,7 @@ function toggle_autoplay_autoforward()
         -- Enable autoplay and disable looping
         mp.set_property("loop-file", "no")
         mp.set_property("keepopen", "no")
-        mp.set_property("image-display-duration", "5")
+        mp.set_property("image-display-duration", "2")
         mp.osd_message("Autoplay & Autoforward: Enabled")
         mp.msg.info("Autoplay and autoforward enabled")
     else
@@ -23,3 +23,4 @@ end
 
 -- Register key binding (Ctrl+a)
 mp.add_key_binding("Ctrl+a", "toggle_autoplay_autoforward", toggle_autoplay_autoforward)
+mp.set_property("image-display-duration", "2")
