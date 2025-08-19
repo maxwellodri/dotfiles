65f6fc4111ec6bb5f26e1754ca795ce9f62bb1c6
Maxwell Odri <maxwellodri@gmail.com>
2025-08-19 15:11:00 +1000

    add rust to installer adn fix dunst syntax

--- installer_main.sh
+++ installer_main.sh
@@ -5,5 +5,6 @@ sh helper_scripts/arch_package_install.sh $1
 sh helper_scripts/makesymlinks.sh "$1"
 sh helper_scripts/custom_bin_scripts.sh
 sh helper_scripts/firefox_user.sh
+sh rust/install.sh
 bash helper_scripts/install_system_configs.sh #after makesymlinks.sh always need $GIT_ROOT/.dotfile_tag file to be present
 bash helper_scripts/download_suckless.sh #Should always be last to provide ssh copying msg, e.g. see script
