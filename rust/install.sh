74f3698e8c928fac299ab0d789ee91f30a9b20bd
Maxwell Odri <maxwellodri@gmail.com>
2025-11-03 17:53:15 +1000

    transferred faucet to its own repository

--- rust/install.sh
+++ rust/install.sh
@@ -3,15 +3,8 @@ if [ -z "$bin" ] || [ -z "$dotfiles" ]; then
     echo "bin var != ~/bin or dotfiles var doesnt exist"
     exit 1
 fi
-
 cd "$dotfiles/rust/dirsort" || exit 1
 echo "Building dir_sort..."
 cargo build --release || exit 1
 ln -sf "$dotfiles/rust/dirsort/target/release/dirsort" "$bin"
 echo "Linked $(realpath "$dotfiles/rust/dirsort/target/release/dirsort") to $bin"
-
-cd "$dotfiles/rust/faucet" || exit 1
-echo "Building faucet..."
-cargo build --release || exit 1
-ln -sf "$dotfiles/rust/faucet/target/release/faucet" "$bin/faucet"
-echo "Linked $(realpath "$dotfiles/rust/faucet/target/release/faucet") to $bin"
