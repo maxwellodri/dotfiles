f448e7491d13c9e0fd46e69671ac68840cc5b081
Maxwell Odri <maxwellodri@gmail.com>
2025-11-09 12:43:26 +1000

    add zram

--- helper_scripts/install_system_configs.sh
+++ helper_scripts/install_system_configs.sh
@@ -349,6 +349,7 @@ print_header "Installing System Configurations"
 copy_files "system_configs/etc/tlp.conf" "/etc" true
 copy_files "system_configs/etc/pacman.d/hooks" "/etc/pacman.d/hooks" true
 copy_files "system_configs/etc/polkit-1/rules.d/" "/etc/polkit-1/rules.d" true
+copy_files "system_configs/etc/systemd/" "/etc/systemd/" true
 
 # Reload configurations
 print_header "Reloading System Configurations"
