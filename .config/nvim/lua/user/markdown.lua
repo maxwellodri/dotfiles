bf48768ebfd3cf195238aeba8a7f6779e73b2e44
Maxwell Odri <maxwellodri@gmail.com>
2026-04-08 00:52:32 +1000

    hide treesitter error highlighting in markdown

--- .config/nvim/lua/user/markdown.lua
+++ .config/nvim/lua/user/markdown.lua
@@ -6,6 +6,7 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.opt_local.softtabstop = 2
         vim.opt_local.tabstop = 2
         vim.opt_local.shiftwidth = 2
+        vim.api.nvim_set_hl(0, '@error', { link = 'Normal' })
     end,
 })
 require('render-markdown').setup({
