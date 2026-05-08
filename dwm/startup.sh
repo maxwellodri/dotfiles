d5e6d4275bdd9e68184150a2568aad4cabf82b99
Maxwell Odri <maxwellodri@gmail.com>
2026-05-08 21:02:49 +1000

    general: turn off autostart nicotine

--- dwm/startup.sh
+++ dwm/startup.sh
@@ -12,10 +12,9 @@ pgrep -xf "transmission-daemon --no-auth" || ( echo "Starting transmission-daemo
 #( which slock && xautolock -time 10 -locker slock) &
 [ "${dotfiles_tag:-}" = "pc" ] && "${bin:-}/steam" &
 [ "${dotfiles_tag:-}" = "pc" ] && signal-desktop &
-[ "${dotfiles_tag:-}" = "pc" ] && spotify-launcher &
 [ "${dotfiles_tag:-}" = "pc" ] && thunderbird &
 [ "${dotfiles_tag:-}" = "pc" ] && (pgrep -x mpd || mpd)
-[ "${dotfiles_tag:-}" = "pc" ] && nicotine &
+#[ "${dotfiles_tag:-}" = "pc" ] && nicotine &
 [ -f "$bin/monitor.sh" ] && "$bin/monitor.sh"
 background_set.sh &
 "$bin/set_kb_map" &
