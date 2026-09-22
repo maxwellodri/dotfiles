a43574ff3f6c67f4b3a947595fb411850061a0ad
Maxwell Odri <maxwellodri@gmail.com>
2026-09-23 04:07:19 +1000

    nvim: tweak md bindings

--- .config/nvim/lua/user/markdown.lua
+++ .config/nvim/lua/user/markdown.lua
@@ -9,6 +9,13 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.opt_local.wrap = true
         vim.opt_local.linebreak = true
         vim.api.nvim_set_hl(0, 'markdownError', { link = 'Normal' })
+        vim.keymap.set('n', 'O', function()
+            local below = vim.fn.line('.')
+            local blank_lines = vim.fn['repeat']({ '' }, vim.v.count1)
+            vim.api.nvim_buf_set_lines(0, below, below, false, blank_lines)
+            vim.api.nvim_win_set_cursor(0, { below + vim.v.count1, 0 })
+            vim.cmd('startinsert!')
+        end, { buffer = true, desc = 'Empty line below' })
     end,
 })
 require('render-markdown').setup({
@@ -180,29 +187,3 @@ require('mkdnflow').setup({
     }
 })
 
-function SmartNewItemDeeper()
-    local ok, lists = pcall(require, 'mkdnflow.lists')
-    if not ok then return end
-    local cur  = vim.api.nvim_win_get_cursor(0)
-    local line = vim.api.nvim_get_current_line()
-    -- figure out what kind of list we are in
-    local list_type = lists.hasListType(line)
-    if not list_type then return end          -- not on a list item
-    local sw = vim.bo.shiftwidth
-    local old_indent = line:match('^%s*') or ''
-    local new_indent = old_indent .. (' '):rep(sw)   -- one level deeper
-    -- ask mkdnflow for the *marker* only (bullet / number)
-    local marker = line:match(lists.patterns[list_type].marker)
-    if not marker then return end
-    -- build the empty item
-    local new_line = new_indent .. marker:gsub('%s+$', '') .. '  '
-    -- insert below current line        
-    vim.api.nvim_buf_set_lines(0, cur[1], cur[1], false, { new_line })
-    -- place cursor after the marker
-    vim.api.nvim_win_set_cursor(0, { cur[1] + 1, #new_line })
-end
-
-vim.keymap.set('n', 'O', function()
-    SmartNewItemDeeper()
-    vim.cmd.startinsert()
-end, { buffer = true, desc = 'Mkdn: deeper item above' })
